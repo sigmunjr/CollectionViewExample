a46981e3559aa51b70199bcdedb4bbeee7351bea
Sigmund Rolfsjord <sigmund.rolfsjord@ciber.com>
2015-07-22 20:37:41 +0200

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Sigmund Rolfsjord on 22.07.15.
+//  Copyright (c) 2015 Sigmund Rolfsjord. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
